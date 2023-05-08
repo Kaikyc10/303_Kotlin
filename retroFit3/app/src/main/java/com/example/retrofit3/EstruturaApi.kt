a398217875e965fedc833b26801f6f2fef4ce9ee
Kaikyc10 <kaikycostatereza@gmail.com>
2023-05-08 20:23:16 -0300

    retrofit3

---
+++ retroFit3/app/src/main/java/com/example/retrofit3/EstruturaApi.kt
@@ -0,0 +1,13 @@
+package com.example.retrofit3
+
+class EstruturaApi {
+
+    var id: Int? = null
+    var ra: String? = null
+    var data: String? = null
+    var lati: String? = null
+    var longi: String? = null
+    var img: String? = null
+
+
+}
